2889f40ccb568a352d86e8641e7c51445d0cc19a
TechBond7 <techbond7@outlook.com>
2023-12-07 16:44:21 +0400

    trait-drop

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {name}!");
 }
 
 fn main() {
-    let m = MyBox::new(String::from("Rust"));
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
 
-    hello(&m);
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointers created.");
 }
